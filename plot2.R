85f0ea8d1e6845a25670a22ff74b0caf696a2e56
kirrinki <kirrinki@gmail.com>
2017-07-07 00:51:39 -0700

    Add files via upload

---
+++ plot2.R
@@ -0,0 +1,23 @@
+# Coursera Data Science: Exploratory Data Analysis
+################################################################################
+## load the data
+rm(list = ls())
+data <- read.table("C:/Users/admin_ithw/Desktop/Data Science Assignments/household_power_consumption.txt", header = T, 
+                   sep = ";", na.strings = "?")
+# convert the date variable to Date class
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+
+# Subset the data
+data <- subset(data, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+# Convert dates and times
+data$datetime <- strptime(paste(data$Date, data$Time), "%Y-%m-%d %H:%M:%S")
+
+# Plot 2
+data$datetime <- as.POSIXct(data$datetime)
+attach(data)
+plot(Global_active_power ~ datetime, type = "l",
+     ylab = "Global Active Power (kilowatts)", xlab = "")
+dev.copy(png, file = "C:/Users/admin_ithw/Desktop/Data Science Assignments/Exploratory Analysis/plot2.png", height = 480, width = 480)
+dev.off()
+detach(data)
